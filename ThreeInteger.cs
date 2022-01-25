c330619620d678d58991d5c4a63afc177e65a751
1000007201 <nishant.sharma5504@gmail.com>
2022-01-25 16:05:36 +0530

    [Nishant] Add:Three Integer program done

--- ThreeInteger.cs
+++ ThreeInteger.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FunctionalProgram
+namespace PracticeProb2
 {
-    internal class ThreeInteger
+    public class ThreeInteger
     {
+        public void FindTriplet(int[] arr, int number)
+        {
+            bool found = false;
+            for (int i = 0; i < number - 2; i++)
+            {
+                for (int j = i + 1; i < number - 1; j++)
+                {
+                    for (int k = j + 1; k < number - 1; k++)
+                    {
+                        if (arr[i] + arr[j] + arr[k] == 0)
+                        {
+                            Console.WriteLine(arr[i] + arr[j] + arr[k]);
+                            found = true;
+
+                        }
+                    }
+                }
+            }
+            if (found == false)
+            {
+                Console.WriteLine("Not Exist");
+            }
+        }
     }
 }
