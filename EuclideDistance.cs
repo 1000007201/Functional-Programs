ce5336873afc289d637055d17cde129d7e29d8ea
1000007201 <nishant.sharma5504@gmail.com>
2022-01-25 16:13:50 +0530

    [Nishant] Add:Euclid Distance program done

--- EuclideDistance.cs
+++ EuclideDistance.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace FunctionalProgram
 {
-    internal class EuclideDistance
+    public class EuclideDistance
     {
+        public void Distance()
+        {
+            Console.WriteLine("Enter value of x and y cordinate");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+        }
     }
 }
