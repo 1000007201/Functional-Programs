71609d23c6a1b07d738d2bf0a80d0c334f83bd23
1000007201 <nishant.sharma5504@gmail.com>
2022-01-25 16:11:27 +0530

    [Nishant] Add:2d Array program done

--- 2dArray.cs
+++ 2dArray.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace FunctionalProgram
 {
-    internal class _2dArray
+    public class MultiArray
     {
+        public void ShowArray()
+        {
+            int[,] arr = new int[3, 2];//Assigning Array
+            //Taking values of array from user
+            Console.WriteLine("Enter values for 3*2 array:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //Displaying array to user
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(" {0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
     }
 }
