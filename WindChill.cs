7c2df8435464a883410db5e7a60939281f77feea
1000007201 <nishant.sharma5504@gmail.com>
2022-01-27 08:10:29 +0530

    [Nishant] Add:Wind chill program added

---
+++ WindChill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    public class WindChill
+    {
+        public void Wind(double temp,double vel)
+        {
+            double w = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.Pow(vel, 0.16);
+            Console.WriteLine(w);
+
+        }
+    }
+}
